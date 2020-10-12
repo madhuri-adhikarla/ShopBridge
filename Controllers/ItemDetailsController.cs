74188f27eaf9dd7154d4d226da2ba251bd4dc0ef
madhuri adhikarla <adhikarlamadhuri66@gmail.com>
2020-10-12 18:43:31 +0530

    code changes

---
+++ Controllers/ItemDetailsController.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShopBridge.Models;
+using ShopBridge.Repositories;
+
+namespace ShopBridge.Controllers
+{
+    public class ItemDetailsController : Controller
+    {
+        private ShopBridgeEntities db = new ShopBridgeEntities();
+
+        // GET: ItemDetails
+        public ActionResult Index()
+        {
+            return View(db.ItemDetails.ToList());
+        }
+
+        /// </summary>
+        /// get item details from the inventory
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemDetail itemDetail = db.ItemDetails.Find(id);
+            if (itemDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_ItemDetails", itemDetail);
+        }
+
+        /// </summary>
+        /// get create item partial view
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView("_CreateItem", new ItemDetail());
+        }
+
+
+        /// </summary>
+        /// create a new item into the inventory
+        /// <param name="itemDetail"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Create(ItemDetail itemDetail)
+        {
+            HttpPostedFileBase file = Request.Files["ImageData"];
+            ItemDetailsRepository repo = new ItemDetailsRepository();
+            int i = repo.SaveDetails(file, itemDetail);
+
+            if (i == 1)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(itemDetail);
+        }
+
+
+        /// </summary>
+        /// Edit the item details
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult EditPartial(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemDetail itemDetail = db.ItemDetails.Find(id);
+            if (itemDetail == null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_EditItemDetails", itemDetail);
+        }
+
+
+        /// </summary>
+        /// Delete the items from inventory
+        /// <param name="itemDetail"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ItemDetail itemDetail)
+        {
+            HttpPostedFileBase file = Request.Files["ImageData"];
+            ItemDetailsRepository repo = new ItemDetailsRepository();
+            if (ModelState.IsValid)
+            {
+                int i = repo.EditDetails(file, itemDetail);
+
+                if (i == 1)
+                {
+                    return RedirectToAction("Index");
+                }
+           
+            }
+            return PartialView("_EditItemDetails", itemDetail);
+        }
+
+
+        /// </summary>
+        /// Delete the items from inventory on confirmation
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public void DeleteConfirmed(int id)
+        {
+            ItemDetail itemDetail = db.ItemDetails.Find(id);
+            db.ItemDetails.Remove(itemDetail);
+            db.SaveChanges();
+            //return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// Retrive Image from database 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult RetrieveImage(int id)
+        {
+            byte[] cover = GetImageFromDataBase(id);
+            if (cover != null)
+            {
+                return File(cover, "image/jpg");
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        /// </summary>
+        /// get image from stream id
+        /// <param name="Id"></param>
+        /// <returns></returns>
+        public byte[] GetImageFromDataBase(int Id)
+        {
+            var q = from temp in db.ItemDetails where temp.ItemId == Id select temp.StreamId;
+            byte[] cover = q.First();
+            return cover;
+        }
+
+
+
+        /// </summary>
+        /// dispose
+        /// <param name="disposing"></param>
+        /// <returns></returns>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Repositories/ItemDetailsRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using ShopBridge.Models;
+
+
+namespace ShopBridge.Repositories
+{
+    public class ItemDetailsRepository
+    {
+        private ShopBridgeEntities db = new ShopBridgeEntities();
+        public int SaveDetails(HttpPostedFileBase file, ItemDetail itemDetail)
+        {
+            itemDetail.StreamId = ConvertToBytes(file);
+            var Content = new ItemDetail
+            {
+                Name = itemDetail.Name,
+                Description = itemDetail.Description,
+                Price = itemDetail.Price,
+                StreamId = itemDetail.StreamId
+            };
+            db.ItemDetails.Add(Content);
+            int i = db.SaveChanges();
+            if (i == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }public int EditDetails(HttpPostedFileBase file, ItemDetail itemDetail)
+        {
+            if(file.InputStream.Length > 0)
+            {
+                itemDetail.StreamId = ConvertToBytes(file);
+            }
+            
+            var Content = new ItemDetail
+            {
+                Name = itemDetail.Name,
+                Description = itemDetail.Description,
+                Price = itemDetail.Price,
+                StreamId = itemDetail.StreamId
+            };
+
+            db.Entry(itemDetail).State = EntityState.Modified;
+            int i = db.SaveChanges();
+            if (i == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+
+        public byte[] ConvertToBytes(HttpPostedFileBase image)
+        {
+            byte[] imageBytes = null;
+            BinaryReader reader = new BinaryReader(image.InputStream);
+            imageBytes = reader.ReadBytes((int)image.ContentLength);
+            return imageBytes;
+        }
+    }
+}
