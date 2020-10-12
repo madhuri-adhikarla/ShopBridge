df93749abcd1334c3d2ba4cbdea7fb037f5ab418
madhuri adhikarla <adhikarlamadhuri66@gmail.com>
2020-10-13 01:08:15 +0530

    unit test and updated codebase

---
+++ TestShopBridge/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestShopBridge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestShopBridge")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cc5b6e1b-d512-4c0a-bd8f-6e1fee9b7900")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestShopBridge/UnitTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShopBridge.Controllers;
+using ShopBridge.Models;
+
+namespace TestShopBridge
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+       public void GetDetails()
+        {
+            int id = 1;
+            ItemDetailsController controller = new ItemDetailsController();
+            ActionResult result =  controller.Details(id);
+            Assert.IsNotNull(result);
+
+        }
+
+        [TestMethod]
+        public void CreateNewItem()
+        {
+            ItemDetail itemDetail = new ItemDetail();
+            itemDetail.Name = "Apple Watch";
+            itemDetail.Description = "Apple Watch Description";
+            itemDetail.Price = 650;
+            ItemDetailsController cnt = new ItemDetailsController();
+            ActionResult result = cnt.Create(itemDetail);
+            Assert.IsNotNull(result);
+        }
+
+        public void editItem()
+        {
+            int id = 3;
+            ItemDetail itemDetail = new ItemDetail();
+            itemDetail.Name = "iPhone";
+            itemDetail.Description = "iPhone Description";
+            itemDetail.Price = 6500;
+            ItemDetailsController cnt = new ItemDetailsController();
+            ActionResult result = cnt.Edit(itemDetail);
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
